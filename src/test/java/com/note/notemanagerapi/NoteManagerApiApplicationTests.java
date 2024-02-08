181d8ddad2f7331f0538603b89ee1f99c5b012c7
Nabil Yahyaoui <yahyaouinabil1993@gmail.com>
2024-02-08 11:03:16 +0100

    initialize project

---
+++ src/test/java/com/note/notemanagerapi/NoteManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.note.notemanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
